f9051b5eea7eda463cb21f02f495a9d91a8bc972
YUNUS <YUNUS@10.0.0.83>
2018-01-31 19:33:38 -0600

    Template changed

--- WelcomeRESTService/src/main/java/com/example/demo/GreetingController.java
+++ WelcomeRESTService/src/main/java/com/example/demo/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String TEMPLATE= "Welcome, %s!";
+	private static final String TEMPLATE= "Hello, We Welcome, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
 	@RequestMapping("/greeting")
